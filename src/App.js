da85313f3615bb9782c99681ecfd03f2825e972d
Alexandre Martins <all.martins076@gmail.com>
2022-11-17 16:46:35 -0300

    Removed unnecessary imports and comments

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import React from 'react';
 import './components/App/App.css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
-import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
-import Preferences from './components/Preferences/Preferences';
-import useToken from './components/App/useToken';
 import ConsultaPorPlaca from './components/ConsultaPorPlaca/ConsultaPorPlaca';
 import NewUser from './components/NewUser/NewUser';
 import Menu from './components/Menu/Menu';
@@ -32,6 +29,7 @@ function App() {
       <h1></h1>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Login />}></Route>  
         <Route path="/login" element={<Login />}></Route>
         <Route path="/menu" element={<Menu />}></Route>
         <Route path="/newuser" element={<NewUser />}></Route>
